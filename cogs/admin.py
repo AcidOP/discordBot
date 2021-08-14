235321467a979531e733430a04b9fde3523758c0
Sk Zeeshan Ali <67377785+acidOP@users.noreply.github.com>
2021-08-14 09:33:40 +0530

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,13 +1,24 @@
 from discord.ext import commands
 
 class admin(commands.Cog):
-  def __init__(self, client):
-    self.client = client
+    def __init__(self, bot):
+        self.bot = bot
 
-  @commands.command()
-  @commands.has_permissions(manage_messages=True)
-  async def clear(self, ctx, amount: int= 1000):
-    await ctx.channel.purge(limit= amount+1)
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def clear(self, ctx, amount: int = 10):
+        await ctx.channel.purge(limit=amount+1)
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def reload(self, ctx, extension):
+        extension = "cogs." + extension
+        try:
+            self.bot.unload_extension(extension)
+            self.bot.load_extension(extension)
+            await ctx.send(f'The extension "{extension}" was reloaded')
+        except:
+            await ctx.send(f'The extension "{extension}" could not be reloaded')
 
 def setup(bot):
-  bot.add_cog(admin(bot))
+    bot.add_cog(admin(bot))
