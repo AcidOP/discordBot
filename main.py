5caccf766615fb45be3410b8f0de9dd4c7a7d2d4
Sk Zeeshan Ali <67377785+acidOP@users.noreply.github.com>
2021-08-14 09:32:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,31 @@
-import os  
-import discord  
-from discord.ext import commands 
+# !/usr/bin/python3
 
-intents = discord.Intents.default()
-intents.members = True
+import os
+import json
+import discord
+from discord.ext import commands
 
-bot = commands.Bot(command_prefix="Pls",
-                   strip_after_prefix=True,
-                   help_command=None,
-                   case_insensitive=True,
-                   intents=intents
-                   )
+bot = commands.Bot(command_prefix="!",case_insensitive=True , intents=discord.Intents.all())
 
-TOKEN = os.environ['token']
-
-def record(author, message):
-	with open('hist.txt', 'w') as hist:
-		hist.write(f'{author} : {message}')
+token = "<discord token>"
 
 @bot.event
-async def on_member_join(member):
-	welcomeChannel = discord.utils.get(bot.get_all_channels(), name='welcome')
-	welcomeEmbed = discord.Embed(
-	    title=f"Welcome {member.name}",
-	    description=f"{member.mention} Thanks for joining!",
-	    color=0x00ff00)
-	welcomeEmbed.set_thumbnail(url=member.avatar_url)
-	await welcomeChannel.send(embed=welcomeEmbed)
-
+async def on_ready():
+    print(f"{bot.user.name} is online!")
 
 @bot.event
 async def on_message(message):
+    # Bot will not reply to the bot itself
+    if message.author == bot.user:
+        return
 
-	if str(message.channel.type) == 'private':
-		modChannel = discord.utils.get(bot.get_all_channels(),
-		                               name='mod-mail')
-		await modChannel.send(f'[{message.author.name}] : {message.content}')
-		return
+    await bot.process_commands(message)
 
-  record(message.author, message.content)
-
-	if message.author == bot.user:
-		return
-
-	await bot.process_commands(message)
+# Load your cogs (if any)
+for file in os.listdir("/path/to/your/cogs"):
+    if file.endswith(".py"):
+        bot.load_extension(f"cogs.{file[:-3]}")
+        print(f"The extension {file} was loaded.")
 
 
-for file in os.listdir('./cogs'):
-	if file.endswith('.py'):
-		bot.load_extension(f'cogs.{file[:-3]}')
-		print(f'The cog {file[:-3]} was loaded')
-
-# Run instance of the bot
-bot.run(TOKEN)
+bot.run(token)
